11e0592620489729c0296b4c6959038c8062a3eb
jp-praveen <55296086+jp-praveen@users.noreply.github.com>
2019-09-16 13:30:32 +0530

    Update minTimeLambertRendezvous.m

--- minTimeLambertRendezvous.m
+++ minTimeLambertRendezvous.m
@@ -155,7 +155,7 @@ else;
     f_dash=((y/C)^1.5)*((1/2*z)*(C-1.5*S/C)+(3*S^2)/(4*C)) + (A/8)*((3*S*sqrt(y)/C)+A*sqrt(C/y));
 end    
     
-while abs((f/f_dash))>0.000001;
+while abs((f/f_dash))>0.01;
     z_f=z-f/f_dash;
     z=z_f;
     S=(1/6)-(z/120)+(z^2/5040)-(z^3/362880)+(z^4/39916800);   
@@ -192,9 +192,12 @@ if delv(1,2)<delv(1,1);
 else
     delv_max=floor(delv(1,1));
 end    
-% Checking Delta V for different True Anomaly, i.e. after a waiting time
-for i=1:40
-    ta_chaser(1,i)=10*i;                                                    % Setting True Anomaly of chaser in degrees
+
+%--------------------------------------------------------------------------------------------------------
+% Checking Delta V for different True Anomaly in a Prograde Orbit
+
+for i=1:80
+    ta_chaser(1,i)=5*i;                                                    % Setting True Anomaly of chaser in degrees
     r(1,i)=(h_c^2)/(u*(1+e*cosd(ta_chaser(1,i))));
     vrc(1,i)=(u*e*sind(ta_chaser(1,i)))/h_c;                                 % Radial velocity of the chaser at that particular true anomaly   
     vpc(1,i)=h_c/r(1,i);                                                    % Perpendicular velocity of the chaser at that True Anomaly    
@@ -252,9 +255,82 @@ for i=1:40
     v2n_t(1,i)=norm(v2_t(:,i));
     
     % Calculating delta V required
-    delv1(1,i)=v1n_t(1,i)-vc(1,i);
-    delv2(1,i)=vd_2-v2n_t(1,i);
-    delv(1,i)=abs(delv1(1,i))+abs(delv2(1,i));
+    delv1_pro(1,i)=v1n_t(1,i)-vc(1,i);
+    delv2_pro(1,i)=vd_2-v2n_t(1,i);
+    delv_pro(1,i)=abs(delv1(1,i))+abs(delv2(1,i));
 end     
+
+%--------------------------------------------------------------------------------------------------------
+% Checking Delta V for different True Anomaly in a Retrograde Orbit
+
+for i=1:80
+    ta_chaser(1,i)=5*i;                                                    % Setting True Anomaly of chaser in degrees
+    r(1,i)=(h_c^2)/(u*(1+e*cosd(ta_chaser(1,i))));
+    vrc(1,i)=(u*e*sind(ta_chaser(1,i)))/h_c;                                 % Radial velocity of the chaser at that particular true anomaly   
+    vpc(1,i)=h_c/r(1,i);                                                    % Perpendicular velocity of the chaser at that True Anomaly    
+    vc(1,i)=sqrt(vpc(1,i)^2+vrc(1,i)^2);
+    rc_cartesian=[r(1,i)*cosd(ta_chaser(1,i));r(1,i)*sind(ta_chaser(1,i));0];
+    ia=95.2886;                                                             % Inclination Angle          
+    DCM=[cosd(o),-sind(o),0;sind(o),cosd(o),0;0,0,1]*[1 0 0;0 cosd(ia) -sind(ia);0 sind(ia) cosd(ia)]*[cosd(w+ta_chaser(1,i)) -sind(w+ta_chaser(1,i)) 0; sind(w+ta_chaser(1,i)) cosd(w+ta_chaser(1,i)) 0; 0 0 1];
+    r1(:,i)=DCM*rc_cartesian;                                    % Writing r1 in ECI frame
+
+    % Choosing a Retrograde trajectory(i>90 deg). Rendezvous occurs at t=T/4 of debri orbit
+    r1cr2=cross(r1(:,i),r2);
+    r1r2=norm(r1(:,i))*norm(r2);
+    if r1cr2(3,1)>=0;
+        deltheta=360-acosd(dot(r1(:,i),r2)/r1r2);
+    else
+        deltheta=acosd(dot(r1(:,i),r2)/r1r2);
+    end    
+    A=sind(deltheta)*sqrt(r1r2/(1-cosd(deltheta)));  
     
-% Should check for unit errors
+    % Starting Newtons method to evaluate the z (z is  related to a and universal anomaly) 
+    z=0;                                                     % Initializing z
+    S=(1/6)-(z/120)+(z^2/5040)-(z^3/362880)+(z^4/39916800);  % S and C are Stumpff Functions 
+    C=(1/2)-(z/24)+(z^2/720)-(z^3/40320)+(z^4/3628800);
+    r1n=norm(r1(1,i));
+    r2n=norm(r2);
+    del_t=t_quarter;
+    y=r1n+r2n+A*((z*S-1)/C^0.5);
+    f=((y/C)^1.5)*S+A*(y^0.5)-(u^0.5)*del_t;
+    if z==0;
+        f_dash=(sqrt(2)/40)*y^1.5+(A/8)*(sqrt(y)+A*sqrt(1/2*y));
+    else;
+        f_dash=((y/C)^1.5)*((1/2*z)*(C-1.5*S/C)+(3*S^2)/(4*C)) + (A/8)*((3*S*sqrt(y)/C)+A*sqrt(C/y));
+    end    
+    while abs((f/f_dash))>0.01;                         % Executing Newtons Methods to find Z which is related to f and g  
+        z_f=z-f/f_dash;
+        z=z_f;
+        S=(1/6)-(z/120)+(z^2/5040)-(z^3/362880)+(z^4/39916800);   
+        C=(1/2)-(z/24)+(z^2/720)-(z^3/40320)+(z^4/3628800);
+        y=r1n+r2n+A*((z*S-1)/C^0.5);
+        f=((y/C)^1.5)*S+A*(y^0.5)-(u^0.5)*del_t;
+        if z==0;
+            f_dash=(sqrt(2)/40)*y^1.5+(A/8)*(sqrt(y)+A*sqrt(1/2*y));
+        else;
+            f_dash=((y/C)^1.5)*((1/2*z)*(C-1.5*S/C)+(3*S^2)/(4*C)) + (A/8)*((3*S*sqrt(y)/C)+A*sqrt(C/y));
+        end
+    end    
+    % Calculating the Lagrange functions and Velocities at r1 and r2
+    f=1-y/r1n;
+    g=(1/sqrt(u))*((y/C)^1.5*S)+A*sqrt(y)-(1/sqrt(u))*(y/C)^1.5;
+    f_dot=(sqrt(u)/r1n*r2n)*sqrt(y/C)*(z*S-1);
+    g_dot=1-(y/r2n);
+    v1_t(:,i)=(r2-f*r1(:,i))/g;                                               % Transfer orbit velocity at r1
+    v2_t(:,i)=(g_dot*r2-r1(:,i))/g;                                           % Transfer orbit velocity at r2
+    v1n_t(1,i)=norm(v1_t(:,i));
+    v2n_t(1,i)=norm(v2_t(:,i));
+    
+    % Calculating delta V required
+    delv1_retro(1,i)=v1n_t(1,i)-vc(1,i);
+    delv2_retro(1,i)=vd_2-v2n_t(1,i);
+    delv_retro(1,i)=abs(delv1(1,i))+abs(delv2(1,i));
+end     
+
+% Plotting Initial true anomaly Vs Delta V required for both trajectory
+
+plot(ta_chaser,delv_pro,'r',ta_chaser,delv_retro,'b');
+title('Initial True Anomaly of Chaser VS Delta V Required');
+xlabel('Initial True Anomaly of Chaser (in degrees)');
+ylabel('Net Delta V Required (in Km/sec)');
+
