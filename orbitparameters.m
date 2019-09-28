a63618a77db8e8a216930a224daebfbc4a725877
jp-praveen <55296086+jp-praveen@users.noreply.github.com>
2019-09-28 13:50:41 +0530

    Update orbitparameters.m

--- orbitparameters.m
+++ orbitparameters.m
@@ -1,6 +1,9 @@
-function [RAAN,inclination,perigee] = orbitparameters(r1,r2,e,Eanomaly_1,Eanomaly_2)
-Tanomaly_1=atand(sqrt((1+e)/(1-e))*tand(Eanomaly_1/2));
-Tanomaly_2=atand(sqrt((1+e)/(1-e))*tand(Eanomaly_2/2));
+function [RAAN,inclination,perigee,Tanomaly_1] = orbitparameters(r1,r2,e,Eanomaly_1,Eanomaly_2)
+Tanomaly_1=2*atand(sqrt((1+e)/(1-e))*tand(Eanomaly_1*0.5))*180/3.14;
+if Tanomaly_1<0;
+    Tanomaly_1=360+Tanomaly_1;
+end
+Tanomaly_2=2*atand(sqrt((1+e)/(1-e))*tand(Eanomaly_2*0.5))*180/3.14
 del_theta=Tanomaly_2-Tanomaly_1;
 if del_theta<180;
     H=cross(r1,r2);
@@ -10,5 +13,8 @@ end
 inclination=atand(sqrt(H(1,1)^2+H(2,1)^2)/H(3,1));
 RAAN=atand(H(1,1)/-H(2,1));
 
-u_t1=atand(r1(1,1)*cosd(inclination)*sind(RAAN)+r1(2,1)*cosd(inclination)*cosd(RAAN)+r1(3,1)*sind(inclination)/r1(1,1)*cosd(RAAN)+r1(2,1)*sind(RAAN));
-perigee=u_t1-Tanomaly_1;
+u_t1=atan2d((-r1(1,1)*cosd(inclination)*sind(RAAN)+r1(2,1)*cosd(inclination)*cosd(RAAN)+r1(3,1)*sind(inclination)),(r1(1,1)*cosd(RAAN)+r1(2,1)*sind(RAAN)));
+perigee=(u_t1-Tanomaly_1);
+if perigee<0;
+    perigee=360+perigee;
+end
