d4927548599910c6e99137fda1ea9328090bf9c7
jp-praveen <55296086+jp-praveen@users.noreply.github.com>
2019-09-28 13:53:06 +0530

    Update lambert.m

--- lambert.m
+++ lambert.m
@@ -1,6 +1,6 @@
 % GIVEN INITAL POSITIONS AND TRANSFER TIME FIND THE VELOCITIES OF THE PROGRADE TRANSFER ORBIT
-function [v1,v2] = lambert(r1,r2,transfer_time)
-u=398588.738;                             % in km^3*s^-2 
+function [v1,v2,RAAN,inclination,perigee,true_anomaly_1,true_anomaly_2] = lambert(r1,r2,transfer_time)
+u=398588.738;                             % in km^3*s^ 
 
 r1cr2=cross(r1,r2);
 r1r2=norm(r1)*norm(r2);
@@ -29,3 +29,40 @@ f_dot=(sqrt(u)/r1n*r2n)*sqrt(y/C)*(z*S-1);
 g_dot=1-(y/r2n);
 v1=(r2-f*r1)/g;                                               % Transfer orbit velocity at r1
 v2=(g_dot*r2-r1)/g;                                           % Transfer orbit velocity at r2
+
+% Calculating the Orbital Elements of the transfer orbit
+evec = ((norm(v1)^2-u/norm(r1))*r1-dot(r1,v1)*v1)/u;
+e = norm(evec)
+v1_r=dot(r1,v1)/r1n;
+if v1_r>=0
+    true_anomaly_1=acosd(dot(evec,r1)/(e*r1n));
+else
+    true_anomaly_1=360-acosd(dot(evec,r1)/(e*r1n));
+end
+v2_r=dot(r2,v2)/r2n;
+if v2_r>=0
+    true_anomaly_2=acosd(dot(evec,r2)/(e*r2n));
+else
+    true_anomaly_2=360-acosd(dot(evec,r2)/(e*r2n));
+end
+h=cross(transpose(r1),transpose(v1));
+K=[0,0,1];
+n=cross(K,h);
+inclination=acosd((h(1,3)/norm(h)));
+RAAN = acosd(n(1,1)/norm(n));
+if n(1,2)<0;
+   RAAN = 360-RAAN;
+end
+
+perigee = acosd(dot(n,evec)/(norm(n)*e));
+
+if evec(3,1)<0;
+    perigee = 360-perigee;
+end
+DCM=[cosd(RAAN),-sind(RAAN),0;sind(RAAN),cosd(RAAN),0;0,0,1]*[1 0 0;0 cosd(inclination) -sind(inclination);0 sind(inclination) cosd(inclination)]*[cosd(perigee) -sind(perigee) 0; sind(perigee) cosd(perigee) 0; 0 0 1];
+   
+   
+v1_norm=norm(v1);
+v2_norm=norm(v2);
+v1_eci=DCM*v1;
+v2_eci=DCM*v2;
