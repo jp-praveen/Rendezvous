76cd6444a220c994de84ce6c446a77e337382c82
jp-praveen <55296086+jp-praveen@users.noreply.github.com>
2019-10-03 01:06:29 +0530

    Update lambert.m

--- lambert.m
+++ lambert.m
@@ -1,56 +1,95 @@
 % GIVEN R1,R2,dT FIND V1,V2 USING PRUSSINGS ALGORITHM
-function [v1,v2,RAAN_trans,inclination_trans,perigee_trans] = lambert(r1,r2,transfer_time)
+function [v1_short,v2_short,RAAN_short,inclination_short,perigee_short,v1_long,v2_long,RAAN_long,inclination_long,perigee_long] = lambert(r1,r2,transfer_time)
 u=398588.738;                             % in km^3*s^-2 
 
 r1n=norm(r1);
 r2n=norm(r2);
 c=r2-r1;
 cn=norm(c);
-s1=(r1n+r2n+cn)
-s2=(r1n+r2n-cn)
+s1=(r1n+r2n+cn);
+s2=(r1n+r2n-cn);
 dt=transfer_time;
 sets1s2(s1,s2,u,dt);
         
-a0=10*r1n;
-%options = optimset('Display','iter'); % show iterations
-at=fsolve(@solve_at,a0)                            % Semi major axis of the transfer orbit
-%at=2.9987e+03;
-%options = optimset('MaxFunEvals',1000)
-%at=fsolve(@solve_at,a0,options);
-%at=7000;
-%delta=5;
-%e_exp=1.0506;
-%h_exp=sqrt(at*u*(e_exp^2-1))
-%at=(at1+at2)*0.5;
-alpha=2*asin(sqrt(s1/(4*at)));
-beta=2*asin(sqrt(s2/(4*at)));
 
-g1=(1-r1n/at)*cot(alpha-beta)-(1-r2n/at)*csc(alpha-beta);
-g2=1-r1n/at;
+%SHORT PATH TRANSFER ORBIT
+a0=100*r1n;
+at_short=fsolve(@solve_at_short,a0);                            % Semi major axis of the transfer orbit
+if imag(at_short)==0 & at_short>s1/4;
+    alpha_short=2*asin(sqrt(s1/(4*at_short)));
+    beta_short=2*asin(sqrt(s2/(4*at_short)));
+
+    g1_short=(1-r1n/at_short)*cot(alpha_short-beta_short)-(1-r2n/at_short)*csc(alpha_short-beta_short);
+    g2_short=1-r1n/at_short;
        
-Eanomaly_trans_1=atan2(g1,g2);
-Eanomaly_trans_2=Eanomaly_trans_1+alpha-beta;
+    Eanomaly_trans_1_short=atan2(g1_short,g2_short);
+    Eanomaly_trans_2_short=Eanomaly_trans_1_short+alpha_short-beta_short;
 
-e_t=(1-r1n/at)/cos(Eanomaly_trans_1)
+    e_short=(1-r1n/at_short)/cos(Eanomaly_trans_1_short);
 
-[RAAN_trans,inclination_trans,perigee_trans,ta_1,ta_2]=orbitparameters(r1,r2,e_t,Eanomaly_trans_1,Eanomaly_trans_2);
-%perigee_trans=37.9619;
-h_trans= sqrt(r1n*u*(1+e_t*cosd(ta_1)))
+    [RAAN_short,inclination_short,perigee_short,ta_1_short,ta_2_short]=orbitparameters(r1,r2,e_short,Eanomaly_trans_1_short,Eanomaly_trans_2_short);
 
-vp1_transfer=h_trans/r1n;                                               % Transfer orbit velocity at r1
-vr1_transfer=(u/h_trans)*e_t*sind(ta_1);
-vp2_transfer=h_trans/r2n;                                               % Transfer orbit velocity at r2
-vr2_transfer=(u/h_trans)*e_t*sind(ta_2);
+    h_short= sqrt(r1n*u*(1+e_short*cosd(ta_1_short)));
 
-%RAAN_trans=-1*RAAN_trans;
-%inclination_trans=-1*inclination_trans;
-%perigee_trans=-1*perigee_trans;
+    vp1_short=h_short/r1n;                                               % Transfer orbit velocity at r1
+    vr1_short=(u/h_short)*e_short*sind(ta_1_short);
+    vp2_short=h_short/r2n;                                               % Transfer orbit velocity at r2
+    vr2_short=(u/h_short)*e_short*sind(ta_2_short);
 
-DCM_trans=[cosd(RAAN_trans),-sind(RAAN_trans),0;sind(RAAN_trans),cosd(RAAN_trans),0;0,0,1]*[1 0 0;0 cosd(inclination_trans) -sind(inclination_trans);0 sind(inclination_trans) cosd(inclination_trans)]*[cosd(perigee_trans+ta_1) -sind(perigee_trans+ta_1) 0; sind(perigee_trans+ta_1) cosd(perigee_trans+ta_1) 0; 0 0 1];
-v1_transfer=[vr1_transfer;vp1_transfer;0];
-v1=DCM_trans*v1_transfer;
+    DCM_short=[cosd(RAAN_short),-sind(RAAN_short),0;sind(RAAN_short),cosd(RAAN_short),0;0,0,1]*[1 0 0;0 cosd(inclination_short) -sind(inclination_short);0 sind(inclination_short) cosd(inclination_short)]*[cosd(perigee_short+ta_1_short) -sind(perigee_short+ta_1_short) 0; sind(perigee_short+ta_1_short) cosd(perigee_short+ta_1_short) 0; 0 0 1];
+    v1_short_1=[vr1_short;vp1_short;0];
+    v1_short=DCM_short*v1_short_1;
 
-DCM_trans=[cosd(RAAN_trans),-sind(RAAN_trans),0;sind(RAAN_trans),cosd(RAAN_trans),0;0,0,1]*[1 0 0;0 cosd(inclination_trans) -sind(inclination_trans);0 sind(inclination_trans) cosd(inclination_trans)]*[cosd(perigee_trans+ta_2) -sind(perigee_trans+ta_2) 0; sind(perigee_trans+ta_2) cosd(perigee_trans+ta_2) 0; 0 0 1];
+    DCM_short=[cosd(RAAN_short),-sind(RAAN_short),0;sind(RAAN_short),cosd(RAAN_short),0;0,0,1]*[1 0 0;0 cosd(inclination_short) -sind(inclination_short);0 sind(inclination_short) cosd(inclination_short)]*[cosd(perigee_short+ta_2_short) -sind(perigee_short+ta_2_short) 0; sind(perigee_short+ta_2_short) cosd(perigee_short+ta_2_short) 0; 0 0 1];
 
-v2_transfer=[vr2_transfer;vp2_transfer;0];
-v2=DCM_trans*v2_transfer;
+    v2_short_1=[vr2_short;vp2_short;0];
+    v2_short=DCM_short*v2_short_1;
+else    
+    v1_short=0;
+    v2_short=0;
+    RAAN_short=nan;
+    inclination_short=nan;
+    perigee_short=nan;
+end
+
+%LONG PATH TRANSFER ORBIT
+a0=100*r1n;
+at_long=fsolve(@solve_at_long,a0);                            % Semi major axis of the transfer orbit
+if imag(at_long)==0 & at_long>s1/4;
+    alpha_long=2*pi-2*asin(sqrt(s1/(4*at_long)))
+    beta_long=2*asin(sqrt(s2/(4*at_long)))
+ 
+    g1_long=(1-r1n/at_long)*cot(alpha_long-beta_long)-(1-r2n/at_long)*csc(alpha_long-beta_long);
+    g2_long=1-r1n/at_long;
+       
+    Eanomaly_trans_1_long=atan2(g1_long,g2_long);
+    Eanomaly_trans_2_long=Eanomaly_trans_1_long+alpha_long-beta_long;
+ 
+    e_long=(1-r1n/at_long)/cos(Eanomaly_trans_1_long)
+ 
+    [RAAN_long,inclination_long,perigee_long,ta_1_long,ta_2_long]=orbitparameters(r1,r2,e_long,Eanomaly_trans_1_long,Eanomaly_trans_2_long);
+ 
+    h_long= sqrt(r1n*u*(1+e_long*cosd(ta_1_long)))
+ 
+    vp1_long=h_long/r1n;                                               % Transfer orbit velocity at r1
+    vr1_long=(u/h_long)*e_long*sind(ta_1_long);
+    vp2_long=h_long/r2n;                                               % Transfer orbit velocity at r2
+    vr2_long=(u/h_long)*e_long*sind(ta_2_long);
+ 
+    DCM_long=[cosd(RAAN_long),-sind(RAAN_long),0;sind(RAAN_long),cosd(RAAN_long),0;0,0,1]*[1 0 0;0 cosd(inclination_long) -sind(inclination_long);0 sind(inclination_long) cosd(inclination_long)]*[cosd(perigee_long+ta_1_long) -sind(perigee_long+ta_1_long) 0; sind(perigee_long+ta_1_long) cosd(perigee_long+ta_1_long) 0; 0 0 1];
+    v1_long_1=[vr1_long;vp1_long;0];
+    v1_long=DCM_long*v1_long_1;
+ 
+    DCM_long=[cosd(RAAN_long),-sind(RAAN_long),0;sind(RAAN_long),cosd(RAAN_long),0;0,0,1]*[1 0 0;0 cosd(inclination_long) -sind(inclination_long);0 sind(inclination_long) cosd(inclination_long)]*[cosd(perigee_long+ta_2_long) -sind(perigee_long+ta_2_long) 0; sind(perigee_long+ta_2_long) cosd(perigee_long+ta_2_long) 0; 0 0 1];
+ 
+    v2_long_1=[vr2_long;vp2_long;0];
+    v2_long=DCM_long*v2_long_1;
+else
+    v1_long=nan;
+    v2_long=nan;
+    RAAN_long=nan;
+    inclination_long=nan;
+    perigee_long=nan;
+end
+
+
