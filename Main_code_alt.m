f4ff0d7ffb03a07b3fe6be063e6a8419289bcd7d
jp-praveen <55296086+jp-praveen@users.noreply.github.com>
2019-10-03 01:05:04 +0530

    Update Main_code_alt.m

--- Main_code_alt.m
+++ Main_code_alt.m
@@ -15,7 +15,7 @@ RAAN=mat(1,2);
 inclination=mat(1,3);
 perigee=mat(1,4);
 
-dv_max=10;
+dv_max=8;
 
 pos_t_ini=[542.13411088 -3934.00754565 -5496.24138485];          % in km
 vel_t_ini=[-2.04410472 5.83405751 -4.77434023];                  % in kmpersec
@@ -39,7 +39,7 @@ rp_chaser=a_chaser*(1-e);                               % Periapsis distance in 
 T_t=(2*pi*a_target^(3/2))/u^0.5;
 T_c=(2*pi*a_chaser^(3/2))/u^0.5;
 i=-1;
-for twait=0.1:100:T_c;                                   % Waiting Time
+for twait=0:100:T_c;                                   % Waiting Time
     i=i+1;
     ma_chaser=(2*pi*twait)/T_c;                        % mean anomaly of chaser in radians
     ma_chaser_deg=ma_chaser*180/pi;
@@ -52,102 +52,59 @@ for twait=0.1:100:T_c;                                   % Waiting Time
         ma_target_deg=ma_target*180/pi;
         ta_target_deg=ma_ta(ma_target_deg);
         r2=tanomaly_rad(ta_target_deg,h_t);
-        
         r1n=norm(r1);
         r2n=norm(r2);
-        c=r2-r1;
-        cn=norm(c);
-        s1=(r1n+r2n+cn);
-        s2=(r1n+r2n-cn);
-        sets1s2(s1,s2,u,dt);
         
-        a0=(a_target+a_chaser);
-        at=fzero(@solve_at,a0);                            % Semi major axis of the transfer orbit
-        
-        if imag(at)==0 & at>s2/4;
-            alpha_rad=2*asin(sqrt(s1/(4*at)));                   
-            beta_rad=2*asin(sqrt(s2/(4*at)));
-            alpha=alpha_rad*180/3.14;
-            beta=beta_rad*180/3.14;
-            g1=(1-r1n/at)*cotd(alpha-beta)-(1-r2n/at)*cscd(alpha-beta);
-            g2=1-r1n/at;
-        
-            Eanomaly_trans_1=atand(g1/g2);
-            Eanomaly_trans_2=Eanomaly_trans_1+alpha-beta;
-            e_t=(1-r1n/at)/cosd(Eanomaly_trans_1);
-            Tanomaly_trans_1=2*atand(sqrt((1+e_t)/(1-e_t))*tand(Eanomaly_trans_1/2));
-            Tanomaly_trans_2=2*atand(sqrt((1+e_t)/(1-e_t))*tand(Eanomaly_trans_2/2));
-            [RAAN_trans,inclination_trans,perigee_trans]=orbitparameters(r1,r2,e_t,Eanomaly_trans_1,Eanomaly_trans_2);
-            DCM_trans=[cosd(RAAN_trans),-sind(RAAN_trans),0;sind(RAAN_trans),cosd(RAAN_trans),0;0,0,1]*[1 0 0;0 cosd(inclination_trans) -sind(inclination_trans);0 sind(inclination_trans) cosd(inclination_trans)]*[cosd(perigee_trans) -sind(perigee_trans) 0; sind(perigee_trans) cosd(perigee_trans) 0; 0 0 1];
-        
-            h_trans= sqrt(r1n*u*(1+e_t*cosd(Tanomaly_trans_1)));
-            h_chaser=sqrt(u*r1n*(1+e));
-            vp1_transfer=h_trans/r1n;                                               % Transfer orbit velocity at r1
-            vr1_transfer=(u/h_trans)*e_t*sind(Tanomaly_trans_1);
-            vp2_transfer=h_trans/r2n;                                               % Transfer orbit velocity at r1
-            vr2_transfer=(u/h_trans)*e_t*sind(Tanomaly_trans_2);
-            v1_transfer=[vr1_transfer;vp1_transfer;0];
-            v1_transfer_eci=DCM_trans*v1_transfer;
-            v2_transfer=[vr2_transfer;vp2_transfer;0];
-            v2_transfer_eci=DCM_trans*v2_transfer;
-        
-            DCM=[cosd(RAAN),-sind(RAAN),0;sind(RAAN),cosd(RAAN),0;0,0,1]*[1 0 0;0 cosd(inclination) -sind(inclination);0 sind(inclination) cosd(inclination)]*[cosd(perigee+ta_target_deg) -sind(perigee+ta_target_deg) 0; sind(perigee+ta_target_deg) cosd(perigee+ta_target_deg) 0; 0 0 1];
-         
-            if ta_target_deg>180; 
-                vp_t=h_t/r2n;                                  % Perpendicular velocity of the target
-                vr_t=(u/h_t)*e*sind(ta_target_deg);            % Radial Velocity of the target 
-            else
-                vp_t=-h_t/r2n;                                  
-                vr_t=-(u/h_t)*e*sind(ta_target_deg);   
-            end
-               
-            vt=[vr_t;vp_t;0];                                   % velocity vector of the target 
-            vt_eci=DCM*vt;
-        
-            DCM=[cosd(RAAN),-sind(RAAN),0;sind(RAAN),cosd(RAAN),0;0,0,1]*[1 0 0;0 cosd(inclination) -sind(inclination);0 sind(inclination) cosd(inclination)]*[cosd(perigee+ta_chaser_deg) -sind(perigee+ta_chaser_deg) 0; sind(perigee+ta_chaser_deg) cosd(perigee+ta_chaser_deg) 0; 0 0 1];
-
-            if ta_chaser_deg>180; 
-                vp_c=h_c/r1n;                                  % Perpendicular velocity of the chaser
-                vr_c=(u/h_c)*e*sind(ta_chaser_deg);            % Radial Velocity of the chaser 
-            else
-                vp_c=-h_c/r1n;                                  
-                vr_c=-(u/h_c)*e*sind(ta_chaser_deg);             
-            end
-        
-            vc=[vr_c;vp_c;0];                        % Velocity Vector of the target
-            vc_eci=DCM*vc;
-        
-            dv1=v1_transfer_eci-vc_eci;
-            dv2=v2_transfer_eci-vt_eci;
-            dv=norm(dv1)+norm(dv2);
-        
-            if dv<dv_max;
-                deltav(1,i+1)=dv;
-                tmin(1,i+1)=t;
-                tcoast(1,i+1)=twait;
-                transfer_time(1,i+1)=dt;
-                true_anomaly_target(1,i+1)=ta_target_deg;
-                r_chaser(:,i+1)=r1;
-                r_target(:,i+1)=r2;
-                trans_velocity_1(:,i+1)=v1_transfer_eci;
-                trans_velocity_2(:,i+1)=v2_transfer_eci;
-                true_anomaly_chaser(1,i+1)=ta_chaser_deg;
+        [v1_short,v2_short,RAAN_short,inclination_short,perigee_short,v1_long,v2_long,RAAN_long,inclination_long,perigee_long]=lambert_prussing(r1,r2,dt);        
                 
-                break;
+        DCM=[cosd(RAAN),-sind(RAAN),0;sind(RAAN),cosd(RAAN),0;0,0,1]*[1 0 0;0 cosd(inclination) -sind(inclination);0 sind(inclination) cosd(inclination)]*[cosd(perigee+ta_target_deg) -sind(perigee+ta_target_deg) 0; sind(perigee+ta_target_deg) cosd(perigee+ta_target_deg) 0; 0 0 1];
+        vp_t=h_t/r2n;                                  % Perpendicular velocity of the target
+        vr_t=(u/h_t)*e*sind(ta_target_deg);            % Radial Velocity of the target 
+        v_target=[vr_t;vp_t;0];                                   % velocity vector of the target 
+        v_target_eci=DCM*v_target;
+        
+        DCM=[cosd(RAAN),-sind(RAAN),0;sind(RAAN),cosd(RAAN),0;0,0,1]*[1 0 0;0 cosd(inclination) -sind(inclination);0 sind(inclination) cosd(inclination)]*[cosd(perigee+ta_chaser_deg) -sind(perigee+ta_chaser_deg) 0; sind(perigee+ta_chaser_deg) cosd(perigee+ta_chaser_deg) 0; 0 0 1];
+        vp_c=h_c/r1n;                                  % Perpendicular velocity of the chaser
+        vr_c=(u/h_c)*e*sind(ta_chaser_deg);            % Radial Velocity of the chaser 
+        v_chaser=[vr_c;vp_c;0];                        % Velocity Vector of the target
+        v_chaser_eci=DCM*v_chaser;
+        
+        dv1_short=v1_short-v_chaser_eci;
+        dv2_short=v2_short-v_target_eci;
+        dv_short_1=norm(dv1_short)+norm(dv2_short);
+        
+        dv1_long=v1_long-v_chaser_eci;
+        dv2_long=v2_long-v_target_eci;
+        dv_long_1=norm(dv1_long)+norm(dv2_long);
+            
+        if dv_short_1<dv_max | dv_long_1<dv_max;
+            if dv_long_1<dv_short_1;
+                dv(1,i+1)=dv_long_1;
+            else 
+                dv(1,i+1)=dv_short_1;
             end
+            tmin(1,i+1)=t;
+            tcoast(1,i+1)=twait;
+            transfer_time(1,i+1)=dt;
+            true_anomaly_target(1,i+1)=ta_target_deg;
+            true_anomaly_chaser(1,i+1)=ta_chaser_deg;
+            r_cha(:,i+1)=r1;
+            r_tar(:,i+1)=r2;
+            r_chaser_norm(:,i+1)=r1n;
+            r_target_norm(:,i+1)=r2n;
+            break;
         end
+        
     end    
 end 
-tmin(tmin==0)=[];
-transfer_time(transfer_time==0)=[];
-tcoast(tcoast==0)=[];
-deltav(deltav==0)=[];
-true_anomaly_target(true_anomaly_target==0)=[];
-true_anomaly_chaser(true_anomaly_chaser==0)=[];
-r_chaser(r_chaser==0)=[];
-r_target(r_target==0)=[];
-trans_velocity_1(trans_velocity_1==0)=[];
-trans_velocity_2(trans_velocity_2==0)=[];
+[a,b]=size(tcoast);
+for k=1:b;
+    if tcoast(1,1)==0 & tmin(1,1)==0 ;
+        tcoast(tcoast==0)=[];
+        tmin(tmin==0)=[];
+        transfer_time(transfer_time==0)=[];
+    end
+end
 plot(tcoast,tmin,'b-o');
 title('Coasting Period VS Tmin');
 xlabel('Coasting time (in seconds)');
