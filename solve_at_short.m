51029fb18a2e301d29cbc169b1886e4c08685cad
jp-praveen <jp.praveen2397@gmail.com>
2019-10-07 13:34:14 +0530

    Finding transfer orbit semi-major axis for Prussings method

---
+++ solve_at_short.m
@@ -0,0 +1,10 @@
+function F = solve_at_short(a_t)
+matrix=gets1s2;
+s1=matrix(1,1);
+s2=matrix(1,2);
+u=matrix(1,3);
+dt=matrix(1,4);
+alpha=2*asin(sqrt(s1/(4*a_t)));                   
+beta=2*asin(sqrt(s2/(4*a_t)));
+
+F(1)=sqrt((a_t(1)^3)/u)*((alpha-sin(alpha))-(beta-sin(beta)))-dt;
