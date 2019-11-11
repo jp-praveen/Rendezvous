b66c1fa86c642cc6d24208e44748a38b6fc5eb25
jp-praveen <jp.praveen2397@gmail.com>
2019-11-11 16:19:33 +0530

    updated on 11/11/19

---
+++ subfolder/sets1s2.m
@@ -0,0 +1,11 @@
+function sets1s2(val1,val2,val3,val4,val5)
+global s1; 
+global s2;
+global u;
+global dt;
+global dtheta;
+s1=val1;
+s2=val2;
+u=val3;
+dt=val4;
+dtheta=val5;
---
+++ subfolder/time_tanomaly.m
@@ -0,0 +1,6 @@
+function [true_anomaly] = time_tanomaly(t,a,e)
+u=398588.738;                                                % in km^3*s^-2  
+T=(2*pi*a^(3/2))/sqrt(u);
+mean_anomaly=(2*pi*t)/T;                                     % mean anomaly in radians 
+mean_anomaly_deg=mean_anomaly*180/3.14;                      % mean anomaly in degrees 
+true_anomaly=ma_ta(mean_anomaly_deg);                        % ta=True Anomaly (in degrees) 
